e92a5f9baba16a0c8f3bd2a530f30a6a1c491544
vijay-soni-Noetic <vkvijaygupta3@gmail.com>
2020-08-05 07:58:40 +0530

    Updates

---
+++ precache-manifest.f7559a6c30df89d8d26feb33b9f648fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7ce832e05c43346035864a241b91ce6",
+    "url": "/list/index.html"
+  },
+  {
+    "revision": "dfe03a57fe24789f316c",
+    "url": "/list/static/css/main.cf684f60.chunk.css"
+  },
+  {
+    "revision": "049153513f995eae2527",
+    "url": "/list/static/js/2.31a444f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/list/static/js/2.31a444f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfe03a57fe24789f316c",
+    "url": "/list/static/js/main.3e7da7c0.chunk.js"
+  },
+  {
+    "revision": "45633e54cf3aadc3a2fd",
+    "url": "/list/static/js/runtime-main.fcc641fe.js"
+  }
+]);
